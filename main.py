3b4a36c9fe83c09cc90e0dcdcb2659d70fb3904a
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-03-12 23:28:36 +0400

    start screen finished

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.gui.OnscreenText import OnscreenText
 from direct.gui.DirectGui import DirectButton
-from panda3d.core import TextNode
-from direct.showbase.DirectObject import DirectObject
+from direct.actor.Actor import Actor
 
 class MyApp(ShowBase):
 
@@ -14,17 +13,31 @@ class MyApp(ShowBase):
             # add an onscreen title that says "Ralph's Patty Panic" in bold
             self.title = OnscreenText(text="Ralph's Patty Panic",
                                       style=1, fg=(1, 1, 1, 1),
-                                      pos=(0, 0.5), scale=0.15)
+                                      pos=(0, 0.7), scale=0.15)
 
             self.setBackgroundColor(0, 0, 0, 1)
             self.button = DirectButton(text=("Start"), scale=0.1, command=self.printHello)
-            self.button.setPos(0, 0, 0)
+            self.button.setPos(0, 0, 0.3)
             self.button['text_fg'] = (0, 1, 1, 1)
             self.button['frameColor'] = (0, 0, 0, 0)
 
             self.name = OnscreenText(text="2024 Abdulaziz Albastaki",
                                      pos=(0, -0.95), fg=(1, 1, 1, 1),
                                      scale=0.07)
+
+            # load ralph model next to the button
+            # make ralph and actor instead of a model
+            self.ralph = Actor("assets/models/ralph",
+                               {"run": "assets/models/ralph-run",
+                                "walk": "assets/models/ralph-walk"})
+            self.ralph.reparentTo(render)
+            self.ralph.setScale(.2)
+            self.ralph.setPos(0, 25, -5)
+            self.ralph.setScale(1)
+            self.ralph.reparentTo(render)
+            # make ralph run constantly
+            self.ralph.loop("run")
+
 
         def printHello(self):
             print('Hello World')
