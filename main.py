82dd8667ce3e8339a0efa827603cc9f81115db7b
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-03-13 11:54:10 +0400

    ralph run

--- main.py
+++ main.py
@@ -36,6 +36,20 @@ class MyApp(ShowBase):
             self.ralph.reparentTo(render)
             self.ralph.loop("run")
 
+            self.keyMap = {
+                "left": False,
+                "right": False
+            }
+
+            self.accept("a", self.updateKey, ["left", True])
+            self.accept("a-up", self.updateKey, ["left", False])
+
+            self.accept("s", self.updateKey, ["backwards", True])
+            self.accept("s-up", self.updateKey, ["backwards", False])
+
+            self.accept("d", self.updateKey, ["right", True])
+            self.accept("d-up", self.updateKey, ["right", False])
+
             # add update to task manager update loop
             self.taskMgr.add(self.update, "update")
         # make an update loop
@@ -68,11 +82,17 @@ class MyApp(ShowBase):
                         self.ralph.setH(self.ralph, 90*dt)
                     else:
                         self.ralph.setH(90)
-                        self.appStatus = "INSTRUCTIONS"
+                        self.appStatus = "GAME"
             elif self.appStatus == "INSTRUCTIONS":
                 self.showInstructions(task)
-            return task.cont
+            if self.appStatus != "GAME":
+                return task.cont
+            else:
+                self.taskMgr.add(self.gameLoop, "update")
+                return task.done
 
+        def updateKey(self, key, value):
+            self.keyMap[key] = value
         def printHello(self):
             print('Hello World')
             self.appStatus = "TRANSITION_GAME"
@@ -86,6 +106,21 @@ class MyApp(ShowBase):
             self.instructions = OnscreenText(text="Press 'SPACE' to go to the next instruction",
                                              pos=(0, -0.95), fg=(1, 1, 1, 1),
                                              scale=0.07)
+
+        def gameLoop(self, task):
+            dt = globalClock.getDt()
+            self.ralph.stop()
+            self.ralph.pose("walk", 0)
+            # make WASD controls for ralph using delta time, moves right and left only
+            if self.keyMap["left"]:
+                self.ralph.setY(self.ralph, 10*dt)
+                self.ralph.loop("run")
+            if self.keyMap["right"]:
+                self.ralph.setY(self.ralph, -10*dt)
+                self.ralph.loop("run")
+            else:
+                self.ralph.stop()
+            return task.cont
 
 app = MyApp()
 app.run()
